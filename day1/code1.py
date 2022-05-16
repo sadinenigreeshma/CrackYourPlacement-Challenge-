c8d8f95424f259b4bda853b02a1d06b6ca02a2e0
sadinenigreeshma <71624353+sadinenigreeshma@users.noreply.github.com>
2022-05-16 12:20:35 +0530

    Update code1.py

--- day1/code1.py
+++ day1/code1.py
@@ -1,1 +1,13 @@
+###https://leetcode.com/problems/find-the-duplicate-number/
 
+class Solution(object):
+def findDuplicate(self, nums):
+"""
+:type nums: List[int]
+:rtype: int
+"""
+for i in range(len(nums)):
+if(nums[0]!=nums[nums[0]]):
+nums[nums[0]],nums[0]=nums[0],nums[nums[0]]
+else:
+return nums[0]
