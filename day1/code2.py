7b06ef34fb27d2bab2b14ccc2dd6e7739a54762c
sadinenigreeshma <71624353+sadinenigreeshma@users.noreply.github.com>
2022-05-16 12:22:12 +0530

    Update code2.py

--- day1/code2.py
+++ day1/code2.py
@@ -1,1 +1,14 @@
+''' https://leetcode.com/problems/remove-duplicates-from-sorted-array/ '''
 
+class Solution(object):
+def removeDuplicates(self, nums):
+"""
+:type nums: List[int]
+:rtype: int
+"""
+count=1
+for i in range(1,len(nums)):
+  if(nums[i-1]!=nums[i]):
+    nums[count]=nums[i]
+    count+=1
+return count
