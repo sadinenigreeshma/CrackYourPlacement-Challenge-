54e8a7162e8248ed8355b57ab9e5a2af3b847959
sadinenigreeshma <71624353+sadinenigreeshma@users.noreply.github.com>
2022-05-16 12:19:13 +0530

    Update code3.py

--- day1/code3.py
+++ day1/code3.py
@@ -1,1 +1,11 @@
 
+###https://leetcode.com/problems/sort-colors/submissions/
+class Solution(object):
+    def sortColors(self, nums):
+        nums.sort()
+        return nums
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        
